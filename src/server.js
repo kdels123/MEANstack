db5567e2b3ad99cbc517ac9fb0c64eb790cc9dec
kdelsener <delsener.k@husky.neu.edu>
2018-06-20 08:18:38 -0400

    updates for heroku

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/webdev-summer1-angular-kdelsener'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/webdev-summer1-angular-kdelsener/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
